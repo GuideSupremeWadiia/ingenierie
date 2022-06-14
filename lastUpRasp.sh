1e7f7da61a7f69c970dc4f637134effdcaa23b01
GuideSupremeWadiia <79404774+GuideSupremeWadiia@users.noreply.github.com>
2022-06-14 15:14:58 +0200

    Update lastUpRasp.sh

--- lastUpRasp.sh
+++ lastUpRasp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in {1. .10}
+for i in {1 2 3 4 5 6 7 8 9 10}
 do
-    touch looser.txt;
-    touch idiot.txt;
+    touch $i+looser.txt;
+    touch $i+idiot.txt;
+    echo "hello";
 done
